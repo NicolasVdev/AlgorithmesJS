75733a386b206639f5ecb85b789d06901496a453
NicolasVdev <nicolasvadelorge.dev@gmail.com>
2023-07-12 19:35:17 +0200

    final push

--- exo02.js
+++ exo02.js
@@ -13,7 +13,7 @@ const algoWestViewDoubleLoop = (building) => {
 
 const list = [3, 7, 8, 3, 6, 1];
 const list2 = [1, 4, 5, 8];
-const list3 = [7, 5, 1, 11, 12, 2, 9, 5, 5, 6];
+const list3 = [20, 5, 1, 11, 12, 2, 9, 5, 5, 6];
 
 console.log(algoWestViewDoubleLoop(list));
 console.log(algoWestViewDoubleLoop(list2));
