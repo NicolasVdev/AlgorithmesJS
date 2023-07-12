78225c24b02bea134c07baba54688de3ed2c27cb
NicolasVdev <nicolasvadelorge.dev@gmail.com>
2023-07-12 18:20:57 +0200

    final push

--- exo04.js
+++ exo04.js
@@ -1,11 +1,14 @@
-const algoWestViewSimpleLoop = (building) => {
-  let count = building.length;
-  for (let i = 0; i < building.length; i++) {
-    for (let j = i + 1; j < building.length; j++) {
-      if (building[i] < building[j]) {
-        count--;
-        break;
-      }
+const algoWestViewSimpleTour = (building) => {
+  let currentIndex = building.length -1;
+  let count = 1;
+  for (i = building.length; i > 1; --i) {
+    console.log("$$$$$$$$$$$$$$$$$$$$$")
+      console.log(i);
+      console.log(currentIndex);
+      console.log("$$$$$$$$$$$$$$$$$$$$$")
+    if (building[i] > building[currentIndex]) {
+      count ++;
+      currentIndex = i;
     }
   }
   return count;
@@ -15,6 +18,19 @@ const list = [3, 7, 8, 3, 6, 1];
 const list2 = [1, 4, 5, 8];
 const list3 = [7, 5, 1, 11, 12, 2, 9, 5, 5, 6];
 
-console.log(algoWestViewSimpleLoop(list));
-console.log(algoWestViewSimpleLoop(list2));
-console.log(algoWestViewSimpleLoop(list3));
+console.log(algoWestViewSimpleTour(list));
+console.log(algoWestViewSimpleTour(list2));
+console.log(algoWestViewSimpleTour(list3));
+
+
+// function westView(data) {
+//   let current = data.length - 1;
+//   let westSide = 1;
+//   for (i = data.length; i > 1; --i) {
+//     if (data[i] > data[current]) {
+//       westSide++;
+//       current = i;
+//     }
+//   }
+//   return westSide;
+// }
